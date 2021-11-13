8adeac22ec8e0b13f49bba2b5726d5a9118d23df
Zorzbleu <pierrot894@gmail.com>
2021-11-13 13:37:09 +0100

    Added functions:
    -create_bucket
    -free_bucket
    -print_bucket
    -print_list_of_buckets
    -create_list_of_buckets
    -free_list_of_buckets
    -get_char_at_pos_in_bucket

--- bucket.c
+++ bucket.c
@@ -15,6 +15,64 @@
 
 #include "bucket.h"
 
+bucket create_bucket(){
+    bucket head = NULL;
+    head = malloc(sizeof(bucket));
+    head->next = NULL;
+    head->val = NULL;
+    return head;
+}
+
+void free_bucket(bucket seau){
+    bucket current = seau;
+    bucket next = seau->next;
+    while (current != NULL) {
+        free(current);
+        if(next != NULL) {
+            current = next;
+            next = next->next;
+        }
+    }
+}
+
+void print_bucket(bucket seau){
+    bucket current = seau;
+    while (current != NULL) {
+        printf("%s\n", current->val);
+        current = current->next;
+    }
+}
+
+void print_list_of_buckets(list_of_buckets liste){
+    int size = sizeof liste / sizeof *liste;
+    for(i=0; i < size; i++){
+        print_bucket(liste[i]);
+    }
+}
+
+
+
+list_of_buckets create_list_of_buckets(int base, int argc, char *argv[]){
+    list_of_buckets liste = NULL;
+    liste = malloc(sizeof(bucket) * base);
+    for (int i = 0; i < base; ++i) {
+        liste[i] = create_bucket();
+    }
+    return liste;
+}
+
+void free_list_of_buckets(list_of_buckets liste){
+    int size = sizeof liste / sizeof *liste;
+    for(i=0; i < size; i++){
+        free_bucket(liste[i]);
+    }
+}
+
+char get_char_at_pos_in_bucket(bucket seau, int pos){
+    char value = seau->val[pos];
+    return value;
+}
+
 int max_lengh_list(char** liste, int lengh){
     int max = 0;
     for(int i = 1; i < lengh; i++){
@@ -43,7 +101,7 @@ bucket remove_head(bucket seau){
 }
 
 bucket move_head(bucket seau_from, bucket seau_to){
-    element * temp;;
+    element * temp;
     add_head(seau_to, seau_from->val);
     temp = remove_head(seau_from);
     return temp;
